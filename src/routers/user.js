cc4e00bf8b8bfdafa099bb0732b61a9e42a0306d
iamayushgupta <ayushgupta71011@gmail.com>
2023-06-27 03:44:44 +0530

    hashin passwords

--- src/routers/user.js
+++ src/routers/user.js
@@ -3,6 +3,8 @@ const router = new express.Router()
 const User = require("../models/user.js")
 const path = require("path")
 const conn = require("../db/mysql.js")
+const bcrypt = require('bcrypt')
+const { AsyncLocalStorage } = require("async_hooks")
 
 router.post('/users/me', async (req, res) => {
     const { name, city, restaurant, favoriteDish } = req.body
@@ -21,18 +23,26 @@ router.get('/users/me', (req, res) => {
     res.sendFile(newPath)
 })
 
+const salt = bcrypt.genSaltSync(8)
 
 router.post("/users/login", async (req, res) => {
     const { email, password } = req.body
-    const query = 'SELECT * FROM users WHERE email = ? AND password = ?'
-    conn.query(query, [email, password], (err, results) => {
+    const query = 'SELECT password FROM users WHERE email = ?'
+    conn.query(query, [email, password], async(err, results) => {
         if (err) {
             res.sendStatus(500)
         }
-        else if (results.length > 0) {
-            res.sendStatus(200)
-        } else {
+        else if (results.length == 0) {
             res.sendStatus(500)
+        } else {
+            const hashedPasswordFromDatabase = results[0].password;
+            const passwordMatch = await bcrypt.compare(password, hashedPasswordFromDatabase);
+
+            if (passwordMatch) {
+                res.sendStatus(200)
+            } else {
+                res.sendStatus(500)
+            }
         }
     })
 })
@@ -44,15 +54,15 @@ router.get("/users/login", async (req, res) => {
 })
 
 
-
-router.post("/users/signup", async (req,res)=>{
-    const {email, password} = req.body
+router.post("/users/signup", async (req, res) => {
+    const { email, password } = req.body
+    const hashedPassword = bcrypt.hashSync(password, salt)
     const query = 'insert into users(email, password) values(?,?)'
-    conn.query(query,[email,password],(err,results)=>{
-        if(err){
+    conn.query(query, [email, hashedPassword], (err, results) => {
+        if (err) {
             res.sendStatus(500)
         }
-        else{
+        else {
             res.sendStatus(200)
         }
     })
@@ -63,4 +73,9 @@ router.get("/users/signup", async (req, res) => {
     res.sendFile(newPath)
 })
 
+
+router.get("/users/home", async (req, res) => {
+    const newPath = path.join(__dirname, "../../views/home.html")
+    res.sendFile(newPath)
+})
 module.exports = router
