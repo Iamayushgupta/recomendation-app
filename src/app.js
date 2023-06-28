c3b3d2b9bb99c32ea49c5d371eaa392b9638b162
iamayushgupta <ayushgupta71011@gmail.com>
2023-06-28 19:03:01 +0530

    added template engine and cleaned the code

--- src/app.js
+++ src/app.js
@@ -5,6 +5,7 @@ require("./db/mongoose.js")
 const app = express()
 const port = process.env.PORT | 3000
 
+app.set('view engine', 'ejs')
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(userRouter)
