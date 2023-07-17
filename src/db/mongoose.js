41c4758f3afd5d1a2a1886900baea1cfb52eb269
iamayushgupta <ayushgupta71011@gmail.com>
2023-07-17 23:13:27 +0530

    Created buttons to view search and update

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,3 +4,5 @@ mongoose.connect('mongodb://127.0.0.1:27017/restaurantDB', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
+
+// /Users/ayushgupta/Documents/mongodb/bin/mongod --dbpath=/Users/ayushgupta/Documents/mongodb-data 
