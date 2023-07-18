6214a6e4d1a7dc2b9e16a91f4b9a3936d8ca4061
iamayushgupta <ayushgupta71011@gmail.com>
2023-07-19 01:58:51 +0530

    updated database-url

--- src/db/mysql.js
+++ src/db/mysql.js
@@ -1,10 +1,18 @@
 const mysql = require('mysql2');
 
+// const conn = mysql.createConnection({
+//   host: 'localhost',
+//   user: 'root',
+//   password : "ayushsql",
+//   database : "restaurantDB"
+// })
+
 const conn = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
+  host: 'database-1.ch9muvve1gk9.ap-south-1.rds.amazonaws.com',
+  user: 'admin',
+  port : 3306,
   password : "ayushsql",
-  database : "restaurantDB"
+  database : "reco_db"
 })
 
 module.exports = conn
