5b5a11409aff756080275f1f806f41488767ecf7
iamayushgupta <ayushgupta71011@gmail.com>
2023-08-30 23:06:01 +0530

    added comments and logs

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,6 +1,6 @@
 const User = require("../models/user.js")
 const bcrypt = require('bcrypt')
-const { sendEmail, generateOTP } = require("../utils/helpers");
+const { sendEmail, generateOTP } = require("../utils/helpers")
 const conn = require("../db/mysql.js")
 
 exports.loginUser = async (req, res) => {
@@ -8,18 +8,21 @@ exports.loginUser = async (req, res) => {
     const query = 'SELECT password FROM users WHERE email = ?'
     conn.query(query, [email, password], async (err, results) => {
         if (err) {
-            res.sendStatus(500)
-        }
-        else if (results.length == 0) {
-            res.sendStatus(500)
+            console.error("Error during login:", err)
+            res.sendStatus(500) // Internal Server Error
+        } else if (results.length === 0) {
+            console.log("User not found:", email)
+            res.sendStatus(500) // Internal Server Error
         } else {
             const hashedPasswordFromDatabase = results[0].password
             const passwordMatch = await bcrypt.compare(password, hashedPasswordFromDatabase)
 
             if (passwordMatch) {
-                res.sendStatus(200)
+                console.log("Login successful:", email)
+                res.sendStatus(200) // OK
             } else {
-                res.sendStatus(500)
+                console.log("Incorrect password for:", email)
+                res.sendStatus(500) // Internal Server Error
             }
         }
     })
@@ -30,78 +33,83 @@ exports.signUpUser = async (req, res) => {
     const salt = bcrypt.genSaltSync(8)
     const hashedPassword = bcrypt.hashSync(password, salt)
 
-    const query = 'insert into users(email, password) values(?,?)'
+    const query = 'INSERT INTO users(email, password) VALUES (?, ?)'
     conn.query(query, [email, hashedPassword], (err, results) => {
         if (err) {
-            res.sendStatus(500)
-        }
-        else {
+            console.error("Error during signup:", err)
+            res.sendStatus(500) // Internal Server Error
+        } else {
             const otp = generateOTP()
             req.session.OTP = otp
             req.session.email = email
             sendEmail(email, otp)
-            res.sendStatus(200)
+            console.log("Signup successful:", email)
+            res.sendStatus(200) // OK
         }
     })
 }
 
 exports.verifyUserOTP = async (req, res) => {
-    console.log(req.session)
-    const {otp} = req.body
-    if (otp==req.session.OTP) {
-        res.sendStatus(200)
+    console.log("Session data:", req.session)
+    const { otp } = req.body
+    if (otp === req.session.OTP) {
+        console.log("OTP verification successful.")
+        res.sendStatus(200) // OK
     } else {
-        res.sendStatus(500)
+        console.log("OTP verification failed.")
+        res.sendStatus(500) // Internal Server Error
     }
 }
 
-exports.registerUser = async(req,res)=>{
+exports.registerUser = async (req, res) => {
     const { name, city, restaurant, favoriteDish } = req.body
     const user = new User({ name, city, restaurant, favoriteDish })
     try {
         await user.save()
-        res.sendStatus(200)
-    }
-    catch (e) {
-        res.sendStatus(500)
+        console.log("User registration successful:", user)
+        res.sendStatus(200) // OK
+    } catch (e) {
+        console.error("Error during user registration:", e)
+        res.sendStatus(500) // Internal Server Error
     }
 }
 
-exports.searchUser = async (req,res)=>{
+exports.searchUser = async (req, res) => {
     try {
         const city = req.query.city
         const name = req.query.name
 
         if (!city) {
             var restaurants = await User.find({ name })
-        }
-        else if (!name) {
+        } else if (!name) {
             var restaurants = await User.find({ city })
-        }
-        else {
+        } else {
             var restaurants = await User.find({ name, city })
         }
 
+        console.log("Search results:", restaurants)
         res.render('search', { restaurants })
     } catch (err) {
-        console.log(err)
+        console.error("Error during search:", err)
     }
 }
 
-exports.editUser = async(req,res) =>{
-    const { id, name, city, restaurant, favoriteDish } = req.body 
+exports.editUser = async (req, res) => {
+    const { id, name, city, restaurant, favoriteDish } = req.body
     try {
         await User.findByIdAndUpdate(id, { name, city, restaurant, favoriteDish }, { new: true, upsert: true })
-        res.sendStatus(200)
-    }
-    catch (e) {
-        res.sendStatus(500)
+        console.log("User information updated:", id)
+        res.sendStatus(200) // OK
+    } catch (e) {
+        console.error("Error during user information update:", e)
+        res.sendStatus(500) // Internal Server Error
     }
 }
 
-exports.resendOTP = async (req,res)=>{
+exports.resendOTP = async (req, res) => {
     const otp = generateOTP()
     req.session.OTP = otp
     sendEmail(req.session.email, otp)
-    res.sendStatus(200)
+    console.log("OTP resent:", req.session.email)
+    res.sendStatus(200) // OK
 }
--- src/models/sqlSchema.sql
+++ src/models/sqlSchema.sql
@@ -1,3 +1,12 @@
--- mysql -u root -p
-CREATE TABLE users (id int NOT NULL AUTO_INCREMENT PRIMARY KEY, email varchar(255) NOT NULL, password varchar(255));
-create index emailIndex on users(email);
+-- To access the MySQL shell with the root user and prompt for the password
+mysql -u root -p
+
+-- Create a table named 'users'
+CREATE TABLE users (
+  id int NOT NULL AUTO_INCREMENT PRIMARY KEY, -- Auto-incrementing primary key
+  email varchar(255) NOT NULL,               -- Email column, cannot be empty
+  password varchar(255)                      -- Password column
+);
+
+-- Create an index named 'emailIndex' on the 'email' column of the 'users' table
+CREATE INDEX emailIndex ON users(email);
